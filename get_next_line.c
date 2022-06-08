09f7b59c78ae160c7841144a951d648a77a38fcc
stena-he <stena-he@student.42wolfsburg.de>
2022-06-08 19:15:35 +0200

    add the start of get_next_line project

---
+++ get_next_line.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: stena-he <stena-he@student.42wolfsburg.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/08 17:52:51 by stena-he          #+#    #+#             */
+/*   Updated: 2022/06/08 17:56:15 by stena-he         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/**
+ * @brief Get the next line object. Returns a line read from a file descriptor.
+ * Read line:  correct behavior. 
+ * NULL: there is nothing else to read, or an error occurred.
+ * 
+ * @param fd The file descriptor to read from.
+ * @return char* 
+ */
+char	*get_next_line(int fd)
+{
+	
+}
---
+++ get_next_line.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: stena-he <stena-he@student.42wolfsburg.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/08 17:51:25 by stena-he          #+#    #+#             */
+/*   Updated: 2022/06/08 17:53:48 by stena-he         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <ctype.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+
+#endif
