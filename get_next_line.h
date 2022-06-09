3c741c6bae8847bd75b1eba91ddef4ba55768c6a
stena-he <stena-he@student.42wolfsburg.de>
2022-06-09 22:44:48 +0200

    update header file

--- get_next_line.h
+++ get_next_line.h
@@ -6,19 +6,20 @@
 /*   By: stena-he <stena-he@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/08 17:51:25 by stena-he          #+#    #+#             */
-/*   Updated: 2022/06/08 17:53:48 by stena-he         ###   ########.fr       */
+/*   Updated: 2022/06/09 22:00:52 by stena-he         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# include <ctype.h>
-# include <stdio.h>
-# include <stdlib.h>
-# include <string.h>
+# include <fcntl.h>
 # include <unistd.h>
+# include <stdio.h>
 
 char	*get_next_line(int fd);
+size_t	ft_strlen(const char *str);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strchr(const char *s, int c);
 
 #endif
